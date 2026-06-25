2cf22eff0020e6d3b5af53d82cc0635a73242682
luvzss <loanloan272819@gmail.com>
2026-06-25 08:23:19 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -11,7 +11,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "dnd",
+    status: "online",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Join https://discord.gg/jordanscreations",
+        name: "gg/jordanscreations",
         // Activity type number (0 = Playing).
         type: 0,
       },
